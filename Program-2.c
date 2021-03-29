c02795311a71267bd5a417f3ac278b869ed8de9b
p1234567717 <20ME0018@vvce.ac.in>
2021-03-29 14:31:31 +0000

    s

--- Program-2.c
+++ Program-2.c
@@ -1,19 +1,20 @@
 // Program to add information of student and display marks
 
 //Input: 10 20 30
- //Output: 10 20 30
+ //Output: 10
 #include<stdio.h>  
 struct student  
 {  //char name[10];
     //int id;
     int marks;  
-};  
+}s[100];  
 int main()  
 {  
-  struct student s1,s2,s3;  
+  struct student s1,s2,s3; 
+  int i; 
   printf("Enter the  marks of student 1,2 and 3");  
 // Read the marks of three students
- 
+ scanf("%d %d %d",&s1.marks,&s2.marks,&s3.marks);
    printf("%d %d %d",s1.marks,s2.marks,s3.marks); 
   return 0;
 }  
